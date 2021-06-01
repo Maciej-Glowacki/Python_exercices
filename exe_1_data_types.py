4c316b1362e238d32764826f25730620ce046ca3
Maciej-Glowacki <maciejglowacki@wp.pl>
2021-06-01 18:49:22 +0200

    exercise 1

---
+++ exe_1_data_types.py
@@ -0,0 +1,15 @@
+num = 1
+flt = 20.21
+word = 'Thor'
+sentence = '''
+Star Wars
+are better
+than Avengers
+'''
+bln = True
+
+print(f'Variable: {num}', '-', type(num))
+print(f'Variable: {flt}', '-', type(flt))
+print(f'Variable: {word}', '-', type(word))
+print(f'Variable: {sentence}', '-', type(sentence))
+print(f'Variable: {bln}', '-', type(bln))
