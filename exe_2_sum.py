578cb81ce6b0146acca3eda6ab769dcde2d05470
Maciej-Glowacki <maciejglowacki@wp.pl>
2021-06-01 19:00:23 +0200

    Exercise 2

---
+++ exe_2_sum.py
@@ -0,0 +1,11 @@
+num_1 = 7
+num_2 = 40.98
+
+sum_value = num_1 + num_2
+
+# function type() returns here type of object
+
+print(f'Variable: {num_1}, type of:', type(num_1))
+print(f'Variable: {num_2}, type of:', type(num_2))
+print(f'Variable: {sum_value}, type of:', type(sum_value))
+
