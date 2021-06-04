09db14b24e8b3dd2a6c020053efb6b796cd0775b
Maciej-Glowacki <maciejglowacki@wp.pl>
2021-06-04 09:55:28 +0200

    Exercises

---
+++ exe_14_name_comparison.py
@@ -0,0 +1,7 @@
+first_name = input('Write your name: ')
+second_name = input('Write your second name: ')
+
+if first_name == second_name:
+    print('Your both names are the same')
+else:
+    print('Your names are different')
---
+++ exe_15_numbers_comparison.py
@@ -0,0 +1,9 @@
+num1 = float(input('Write your first number: '))
+num2 = float(input('Write your second number: '))
+
+if num1 < num2:
+    print('Your first number is lower than second')
+elif num1 == num2:
+    print('Your numbers are equal')
+else:
+    print('Second number is lower than first')
