47af72a1aca65ffaaccac9cd89813bdbfafbf844
Maciej-Glowacki <maciejglowacki@wp.pl>
2021-07-13 18:22:45 +0200

    Exercises

---
+++ exe_37_func_square_area.py
@@ -0,0 +1,5 @@
+def square_area(num1, num2):
+    result = num1 * num2
+    return result
+
+print(square_area(2, 8))
---
+++ exe_38_func_circle_circuit.py
@@ -0,0 +1,5 @@
+def circle_circuit(diameter, pi = 3.14):
+    result = 2 * diameter * pi
+    return round(result, 2)
+
+print(circle_circuit(5))
