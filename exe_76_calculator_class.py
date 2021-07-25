f5acc393b6bf5bf99d94418b633d3b67717de1e4
Maciej-Glowacki <maciejglowacki@wp.pl>
2021-07-25 15:43:21 +0200

    Exercises

---
+++ exe_76_calculator_class.py
@@ -0,0 +1,29 @@
+class Calculator:
+    def __init__(self):
+        self.operation = []
+
+    def add(self, num1, num2):
+        self.result = num1 + num2
+        self.operation.append(f'added {num1} to {num2} got {self.result}')
+        return self.result
+
+    def multiply(self, num1, num2):
+        self.result = num1 * num2
+        self.operation.append(
+            f'multiplied {num1} with {num2} got {self.result}')
+        return self.result
+
+    def print_operations(self):
+        print('Current operations:')
+        for operation in self.operation:
+            print(operation)
+
+
+myCalc = Calculator()
+
+myCalc.add(2, 3)
+myCalc.add(12, 3)
+myCalc.add(22, 3)
+myCalc.add(32, 3)
+myCalc.multiply(23, 2)
+myCalc.print_operations()
