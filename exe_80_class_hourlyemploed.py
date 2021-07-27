249cf3711e09e83ab3bd79c17575a03837e47aca
Maciej-Glowacki <maciejglowacki@wp.pl>
2021-07-27 10:45:17 +0200

    Exercises

--- exe_80_class_hourlyemploed.py
+++ exe_80_class_hourlyemploed.py
@@ -1,11 +1,15 @@
 from exe_78_class_employee import Employee
 
-class HourlyEmploee(Employee):      
+class HourlyEmploee(Employee):
+    def __init__(self, id, first_name, last_name, hours):
+        super().__init__(id, first_name, last_name) 
+        self.hours = 0 
+            
     def compute_payment(self, hours):
         self.hours = hours
         return f'Employee {self.first_name} {self.last_name} earns {self._salary * self.hours} złotych for {self.hours} already worked hours'
 
 
-c = HourlyEmploee(2, 'John', 'Doe')
+c = HourlyEmploee(2, 'John', 'Doe', 12)
 c.set_salary(50)
 print(c.compute_payment(12))
---
+++ exe_81_class_SalariedEmploee.py
@@ -0,0 +1,9 @@
+from exe_78_class_employee import Employee
+
+class SalariedEmployee(Employee):
+    def
+
+# Stwórz klasę SalariedEmployee, która ma reprezentować pracownika z którym pracodawca ma umowę miesięczną. Klasa ma spełniać następujące wymogi:
+
+# Dziedziczyć po klasie Employee.
+# Mieć dodatkową metodę compute_payment(), która będzie zwracała kwotę do wypłacenia pracownikowi za miesiąc (dla uproszczenia możemy założyć że w każdym miesiącu jest 190 godzin pracujących).
